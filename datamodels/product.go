d5f016e0712e4e53c3bff97c86974b56d4ff2920
Heather <2428792711@qq.com>
2021-07-04 17:43:54 +0800

    product

--- datamodels/product.go
+++ datamodels/product.go
@@ -1,1 +1,9 @@
 package datamodels
+
+type Product struct {
+	ID           int64  `json:"id" sql:"ID" imooc:"id"`
+	ProductName  string `json:"ProductName" sql:"productName" imooc:"ProductName"`
+	ProductNum   int64  `json:"ProductNum" sql:"ProductNum" imooc:"ProductNum"`
+	ProductImage string `json:"ProductImage" sql:"ProductImage" imooc:"ProductImage"`
+	ProductUrl   string `json:"ProductUrl" sql:"ProductUrl" imooc:"ProductUrl"`
+}
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/kataras/iris/v12 v12.1.8
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/go-sql-driver/mysql v1.6.0
 )
